6ca65d2b0ebf12e4eac6f0a6cc57a9ee7adf50d2
chaesanamidaarumdapta <113958419+chaesanamidaarumdapta@users.noreply.github.com>
2022-09-20 10:32:23 +0700

    Add files via upload

---
+++ latihan 2 algoritma.py
@@ -0,0 +1,7 @@
+#Chaesa Namida - 064002200008
+print("Hitung Luas Lingkaran")
+a = float(input("Masukkan Panjang Ruangan"))
+b = float(input("Masukkan Lebar Ruangan"))
+satuan = input("Masukkan satuan(Meter/Inci):")
+luas = a*b
+print("luas ruangan dengan panjang:",a, "dan lebar",b, "adalah", (luas), satuan)
